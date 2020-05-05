5b31450bd2be69a15fd174ff63689cde758ba934
Grzegorz Mierzwa <mierzwagrzegorz@gmail.com>
2020-05-05 23:57:13 +0200

    Add material UI

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import PageNav from '../PageNav/PageNav';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import styles from './MainLayout.module.scss';
+import Container from '@material-ui/core/Container';
 
 
 const MainLayout = ({children}) => {
 
   return (
-    <div>
-      <PageNav/>
-      {children}
+    <div className={styles.component}>
+      <AppBar>
+        <Container maxWidht='lg'>
+          <Toolbar disableGutters>
+            <PageNav/>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Container maxWidht='lg'>
+        <Toolbar />
+        {children}
+      </Container>
     </div>
   );
 };
