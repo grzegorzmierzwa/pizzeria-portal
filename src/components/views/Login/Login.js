98f256f36ff077e346d5291b3eac13c08b432be9
Grzegorz Mierzwa <mierzwagrzegorz@gmail.com>
2020-05-17 23:57:22 +0200

    Add view login

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import styles from './Login.module.scss';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
 
 
 const Login = () => {
   return (
     <div className={styles.component}>
-      <h2>Login view</h2>
+      <TextField className={styles.input} id="standard-basic" label="Login" variant="outlined"/>
+      <TextField
+        className={styles.input}
+        id="standard-password-input"
+        label="Password"
+        type="password"
+        autoComplete="current-password"
+        variant="outlined"
+      />
+      <Button className={styles.button} variant="contained" color="primary">
+          Sing in
+      </Button>
     </div>
   );
 };
